98466341d01fbad3e585662a056c93d4ad8c04a4
sachinmane4414 <sachinmane1222@gmail.com>
2023-11-29 20:12:08 +0530

    updated

--- setup.py
+++ setup.py
@@ -8,14 +8,10 @@ HYPEN_E_DOT="-e."
 def get_requirements()->List[str]:
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
         requirement_list=requirement_file.readlines()
-    requirement_list=[requirment_name.replace("\n","")for requirement_name in requirement_list]
+    requirement_list=[requirement_name.replace("\n","")for requirement_name in requirement_list]
     if HYPEN_E_DOT in requirement_list:
         requirement_list.remove(HYPEN_E_DOT)
     return requirement_list
-
-
-
-
 
 
 setup(
