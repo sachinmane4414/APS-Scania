ace0c9f526a4dc1bc71540d21b012dea3a675b32
sachinmane4414 <sachinmane1222@gmail.com>
2023-11-29 19:29:27 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages,setup
+
+
+def get_requirements():
+    pass
+
+
+
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="sachin",
+    author_email="sachinmane1222@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
